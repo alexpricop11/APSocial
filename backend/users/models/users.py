218a5252f4d8b244209d462853ac3c1967d750fb
Alex Pricop <Pricop>
2024-09-10 23:31:33 +0300

    add online in user_profile

--- backend/users/models/users.py
+++ backend/users/models/users.py
@@ -28,7 +28,6 @@ class UserManager(BaseUserManager):
 class Users(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=20, unique=True)
-    online = models.BooleanField(default=False)
     password = models.CharField(max_length=100)
     email = models.EmailField(unique=False, blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
--- backend/users/serializers/profile.py
+++ backend/users/serializers/profile.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from users.models.users import Users
 
+from users.models.user_online import UserOnline
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    online = serializers.SerializerMethodField()
+
     class Meta:
         model = Users
-        fields = ['id', 'username', 'my_followers', 'follow', 'birthday', 'email']
+        fields = ['id', 'username', 'my_followers', 'follow', 'birthday', 'email', 'online']
+
+    @staticmethod
+    def get_online(user):
+        online_status = UserOnline.objects.get(user=user)
+        return online_status.is_online
 
 
 class EditUserProfileSerializer(serializers.ModelSerializer):
